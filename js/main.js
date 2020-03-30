e27b5db0c371a4122fc568452d42cc300b9cc187
Yassin Hussein <4assin@gmail.com>
2020-03-30 22:18:12 +0200

    adding dark mode feature

--- js/main.js
+++ js/main.js
@@ -33,3 +33,33 @@
       seachOverly.classList.remove("open-search-overly")
      }
 
+
+  /************************************/
+  /*************Dark Mode**************/
+  /************************************/
+
+  const toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
+
+   function switchTheme(e) {
+      if (e.target.checked) {
+          document.documentElement.setAttribute('data-theme', 'dark');
+          localStorage.setItem('theme', 'dark');
+      }
+      else {
+          document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light')
+      }    
+  } 
+
+   toggleSwitch.addEventListener('change', switchTheme, false);
+
+
+   const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
+}
